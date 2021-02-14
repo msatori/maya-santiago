45f13e526ce2afec36984d2b790b15dedf062c05
Maya Santiago <msatori.code@gmail.com>
2021-02-14 01:33:50 -0600

    refactored App.js/changed background

---
+++ maya-santiago/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+//component imports
+import About from './components/About';
+//style imports
+
+
+function App() {
+  return (
+    <div>
+      <main>
+        <About></About>
+      </main>
+    </div>
+  );
+}
+
+export default App;
---
+++ maya-santiago/src/components/About/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section>
+            <h1  id="about">It's Me, Maya</h1>
+        </section>
+    )
+}
+
+export default About;
