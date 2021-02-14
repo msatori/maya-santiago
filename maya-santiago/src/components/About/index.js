1e4f12d993a69f4ba8721eb1a3e1c1761d986cd1
Maya Santiago <msatori.code@gmail.com>
2021-02-14 01:57:08 -0600

    created basic about component

--- maya-santiago/src/components/About/index.js
+++ maya-santiago/src/components/About/index.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 function About() {
-    return(
-        <section>
-            <h1  id="about">It's Me, Maya</h1>
+    return (
+        <section className="my-5">
+            <h1 id="about">It's Me, Maya</h1>    <div className="my-2">
+                <p>
+                    Born and raised in the midwest, I have set out to bring the artistry of my corner of Earth to the world through digital art. Join me on my journey.
+      </p>
+            </div>
         </section>
-    )
+    );
 }
 
 export default About;
