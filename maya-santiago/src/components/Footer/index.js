8ef054a996ccd5b15fe11e0192a71d16ecf02e55
Maya Santiago <msatori.code@gmail.com>
2021-02-15 13:58:26 -0600

    linkedin link not working

--- maya-santiago/src/components/Footer/index.js
+++ maya-santiago/src/components/Footer/index.js
@@ -7,15 +7,15 @@ const Footer = () => {
         <section className="footer">
             <div className="footer-container">
                 <h5 className="footer-text">
-                    Made by Satori Code
+                    Built by Satori Code
                 </h5>
             </div>
             <div className="footer-icons">
-                <a href="www.linkedin.com/in/maya-santiago-58565a114" target="blank">
-                    <FontAwesomeIcon icon={faLinkedin} />
-                </a>
                 <a href="https://github.com/msatori" target="blank">
                     <FontAwesomeIcon icon={faGithub} />
+                </a>
+                <a href="www.linkedin.com/in/maya-santiago-58565a114" target="blank">
+                    <FontAwesomeIcon icon={faLinkedin} />
                 </a>
             </div>
         </section>
