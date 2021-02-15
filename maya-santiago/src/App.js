cc2b3fc602af29bc84e61eed40b4842278da4a6c
Maya Santiago <msatori.code@gmail.com>
2021-02-14 22:20:59 -0600

    basic styling

--- maya-santiago/src/App.js
+++ maya-santiago/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 //component imports
 import About from './components/About';
+import Nav from './components/Nav';
 //style imports
 
 
 function App() {
   return (
-    <div>
+    <div className="container-fluid">
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
---
+++ maya-santiago/src/components/Nav/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+
+function Nav() {
+
+    const clickHander = (item) => {
+        console.log(item)
+        return item;
+    }
+    return(
+        <header className="flex-row px-1">
+            <h2>
+                <a href="/">
+                    Maya Santiago
+                </a>
+            </h2>
+            <nav>
+                <ul className='flex-row'>
+                    <li className='mx-2'>
+                       <a href="#about" onClick={clickHander}> About Me </a>
+                    </li>
+                    <li className='mx-2'>
+                    <a href="#projects" onClick={clickHander}> My Projects</a>
+                    </li>
+                    <li className="mx-2">
+                    <a href="#contact" onClick={clickHander}> Contact Me </a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+    )
+};
+
+export default Nav;
