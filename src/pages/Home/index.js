3faef03e2c14713c58d401f48d90c1809678d5f9
Maya Santiago <msatori.code@gmail.com>
2021-03-21 22:36:26 -0500

    changed background, made pages folder

---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <section className="home-container">
+        <h1 id="about">Hi, <br /> I'm Maya.</h1>
+
+        <span className="about-span font-effect-3d-float">Nice to meet you.</span>
+        </section>
+    )
+}
